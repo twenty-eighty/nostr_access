0d0b0224766bec028a5f77753c0430f926c7053f
Twenty Eighty <twenty-eighty@xn--y9a.net>
2025-10-25 21:29:57 +0200

    version update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NostrAccess.MixProject do
   def project do
     [
       app: :nostr_access,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
