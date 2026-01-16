1f59deb79a3a11428c23c0f0832058da9e9c7787
Twenty Eighty <twenty-eighty@xn--y9a.net>
2026-01-16 17:28:10 -0500

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NostrAccess.MixProject do
   def project do
     [
       app: :nostr_access,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
